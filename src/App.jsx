f871998b73c459aac33ae795c165b8394b7abc9f
Dharamraj <dpy9572@gmail.com>
2025-06-20 10:40:10 +0530

    Push Frontend

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EditorReviewer from './Components/EditorReviewer'
+
+const App = () => {
+  return (
+    <>
+    <EditorReviewer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/EditorReviewer.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import "prismjs/themes/prism-tomorrow.css";
+import Prism, { languages } from "prismjs";
+import "prismjs/components/prism-jsx";
+import Editor from "react-simple-code-editor";
+import axios from 'axios'
+import Markdown from 'react-markdown'
+
+const EditorReviewer = () => {
+  const [code, setCode] = useState(``);
+  const [review, setReview] = useState(``)
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
+
+
+  const codeReview = async ()=>{
+  const response =  await axios.post('https://ai-power-code-review-backend.onrender.com/ai/get-response/', {prompt: code});
+  // console.log(response.data);
+  setReview(response.data)
+  }
+
+
+  return (
+    <main className="h-screen flex flex-row p-2 gap-5 bg-zinc-950">
+      {/* Code */}
+      <div className="h-full w-1/2 rounded-2xl bg-gray-900 relative text-xl drop-shadow-amber-50">
+        <div className="h-full w-full">
+          <Editor
+            className="rounded-2xl text-xl border-[1px] border-zinc-300  h-full w-full "
+            value={code}
+            onValueChange={(code) => setCode(code)} 
+            highlight={(code) =>
+              Prism.highlight(code, Prism.languages.javascript, "javascript")
+            }
+            placeholder="Enter Code Here"
+            required
+            padding={16}
+            style={{ backgroundColor: "#111827", fontFamily: "monospace", margin: 0, color: "#fff",}}
+          />
+
+          {/* <pre
+            className="h-full w-full overflow-auto rounded-xl"
+            style={{ backgroundColor: "#111827", margin: 0 }}
+          >
+            <code className="language-jsx whitespace-pre block">
+              {
+
+              }
+            </code>
+          </pre> */}
+        </div>
+
+          <button 
+          onClick={codeReview}
+          className="absolute bottom-2 right-2 px-4 py-2 active:scale-95 bg-sky-400 rounded-2xl font-medium cursor-pointer select-none">
+            Send
+          </button>
+      </div>
+
+      {/* Review */}
+      <div className="h-full w-1/2 bg-gray-800 rounded-2xl   p-4 text-xl drop-shadow-amber-50 overflow-auto text-zinc-100">
+        <button 
+          className="px-4 py-2 bg-sky-400 mb-2 font-medium select-none">
+            Review
+          </button>
+      <Markdown>{review}</Markdown>
+      </div>
+    </main>
+  );
+};
+
+export default EditorReviewer;
